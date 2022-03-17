a054e8933abfa954f0e952b85840928bacc3ffa2
mcooper7649 <55610025+mcooper7649@users.noreply.github.com>
2022-03-16 23:41:07 -0400

    setup form with handlers and configured get/post api routes

---
+++ pages/api/feedback.js
@@ -0,0 +1,41 @@
+import fs from 'fs';
+import path from 'path';
+
+function buildFeedbackPath() {
+  return path.join(process.cwd(), 'data', 'feedback.json');
+}
+
+function extractFeedback(filePath) {
+  const fileData = fs.readFileSync(filePath);
+  const data = JSON.parse(fileData);
+  return data;
+}
+
+function handler(req, res) {
+  if (req.method === 'POST') {
+    const email = req.body.email;
+    const feedbackText = req.body.text;
+
+    const newFeedback = {
+      id: new Date().toISOString(),
+      email: email,
+      text: feedbackText,
+    };
+
+    //store in a db or file
+    const filePath = buildFeedbackPath();
+    const data = extractFeedback();
+    data.push(newFeedback);
+    fs.writeFileSync(filePath, JSON.stringify(data));
+    res.status(201).json({ message: 'Success!', feedback: newFeedback });
+  } else {
+    const filePath = buildFeedbackPath();
+    const data = extractFeedback(filePath);
+
+    res.status(200).json({
+      feedback: data,
+    });
+  }
+}
+
+export default handler;
